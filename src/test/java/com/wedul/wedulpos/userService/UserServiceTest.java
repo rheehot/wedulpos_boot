874c7d3012b4121d5b3fa538913004e7992b5cf1
wedul <rokking2@gmail.com>
2018-06-10 09:10:01 +0900

    remove test code
    테스트코드 제거

--- src/test/java/com/wedul/wedulpos/userService/UserServiceTest.java
+++ src/test/java/com/wedul/wedulpos/userService/UserServiceTest.java
@@ -2,13 +2,10 @@ package com.wedul.wedulpos.userService;
 
 import org.junit.Test;
 
-import com.wedul.common.util.AES256Cipher;
-
 public class UserServiceTest {
 	
 	@Test
 	public void getAesString() {
-		System.out.println(AES256Cipher.getInstance().AES_Encode("GomChul5848!@"));;
 	}
 
 }
